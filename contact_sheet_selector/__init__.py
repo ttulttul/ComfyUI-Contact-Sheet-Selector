1b7e16e1b1d6a88a5db5ab0fc5b87f411bafba29
Ken Simpson <ksimpson@ttul.org>
2025-11-16 11:45:04 -0800

    Expose node mappings for ComfyUI

--- contact_sheet_selector/__init__.py
+++ contact_sheet_selector/__init__.py
@@ -1,9 +1,20 @@
-from .node import ContactSheetExtension, ContactSheetSelector
+from .node import ContactSheetExtension, ContactSheetSelector, comfy_entrypoint
 
 WEB_DIRECTORY = "./web"
+
+NODE_CLASS_MAPPINGS = {
+    "ContactSheetSelector": ContactSheetSelector,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ContactSheetSelector": "Contact Sheet Selector",
+}
 
 __all__ = [
     "ContactSheetSelector",
     "ContactSheetExtension",
     "WEB_DIRECTORY",
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    "comfy_entrypoint",
 ]
